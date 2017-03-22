1579c06e9ff6037c18ef8b7ddeb58e50d67e3f4f
John Shepler <john.sheplerjr@gmail.com>
2017-03-22 13:17:08 -0600

    atempt to pull things into gitignore

--- public/scripts/auth0-variables.js
+++ public/scripts/auth0-variables.js
@@ -1,4 +1,3 @@
-
-var AUTH0_CLIENT_ID='7rC5bCOIFS04j08SeFAygi7fvdCGoeK9';
-var AUTH0_DOMAIN='shep222.auth0.com';
+var AUTH0_CLIENT_ID= process.env.AUTH0_CLIENT_ID
+var AUTH0_DOMAIN= process.env.AUTH0_DOMAIN
 var AUTH0_CALLBACK_URL='https://wedj-youtube.firebaseapp.com/dashboard.html';
