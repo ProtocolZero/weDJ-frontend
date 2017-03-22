8763406dad497547de959b3c0db62c9c60cdf427
DariaDesiderata <daria.calwell@gmail.com>
2017-03-22 11:14:00 -0600

    add dynamic dashboard preview

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -16,18 +16,30 @@ $().ready(() => {
 //3. make a call for playlist_song to get id of songs[0] for a specific playlist
 //.4 make a call for song to get album_image
 function showPlaylists(playlists) {
-console.log(playlists);
   playlists.forEach(playlist => {
     $('.playlist-area').append(
-      `<div class="card-image-dc">
-          <img src="./stylesheets/party.jpg">
+      `<div class="col l6 m6 s12 card-image-dc">
+          <img class="album-image" id="${playlist.id}" src="./stylesheets/party.jpg">
           <span><a class="plist-name-${playlist.id}" href="${url}/playlist/${playlist.id}">${playlist.title}</a></span>
           <p>Role: ${playlist.role}</p>
-          <a class="btn-floating halfway-fab waves-effect waves-light"><i class="material-icons">add</i></a>
-        </div>  `)
+          <a class="btn-floating edit halfway-fab waves-effect waves-light"><i class="material-icons">edit</i></a>
+          `)
+    $.get(`${url}/playlist_song/playlist/${playlist.id}`)
+      .then(songs => {
+        let firstSongId = songs[0].s_id
+        // let pId = songs[0].p_id
+        $.get(`${url}/song/${firstSongId}`)
+          .then(song => {
+               appendImage(song[0], playlist.id)
+          })
+      })
   })
 }
 
+function appendImage(song, id) {
+  const imgUrl = song.album_img
+  $(`#${id}`).attr('src', imgUrl)
+}
 
 
 
