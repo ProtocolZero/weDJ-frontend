8b676c466e467c94cc69e31e43a849b58b3cf6a9
DariaDesiderata <daria.calwell@gmail.com>
2017-03-24 10:15:28 -0600

    change button

--- public/scripts/playlist.js
+++ public/scripts/playlist.js
@@ -38,7 +38,7 @@ $().ready(() => {
           <button class="btn waves-effect waves-light change-song right" value="${song.URL}">Play</button>
          </td>
          <td>
-           <button class="btn waves-effect waves-light"><i class="material-icons">thumb_up</i>/button>
+           <button class="btn waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
          </td>
          <td>
            <button class="btn waves-effect waves-light"><i class="material-icons">thumb_down</i></button>
