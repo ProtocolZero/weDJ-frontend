b56e9dc57f9889cb50fc97806afec46d4dd300e9
John Shepler <john.sheplerjr@gmail.com>
2017-03-24 10:21:25 -0600

    update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,25 +3,8 @@ window.addEventListener('load', function() {
 
     var lock = new Auth0Lock('7rC5bCOIFS04j08SeFAygi7fvdCGoeK9', 'shep222.auth0.com', {
         container: 'root',
-        // auth: {
-        //     // redirectUrl: 'http://localhost:5000/dashboard.html',
-        //     responseType: 'code',
-        //     params: {
-        //         scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
-        //     }
-        // }
     });
     lock.show();
-
-
-    // buttons
-    // btn_login.addEventListener('click', function() {
-    //     lock.show();
-    // });
-
-    // btn_logout.addEventListener('click', function() {
-    //     logout();
-    // });
 
     lock.on("authenticated", function(authResult) {
         lock.getUserInfo(authResult.accessToken, function(error, profile) {
@@ -29,6 +12,15 @@ window.addEventListener('load', function() {
                 // Handle error
                 return;
             }
+            console.log('HERE', profile.email);
+
+            $.post('https://wedj.herokuapp.com/user', {
+              email: profile.email,
+              username: profile.email
+            }).then(function (data){
+              console.log("You Posted the UserName");
+            })
+
 
             localStorage.setItem('access_token', authResult.accessToken);
             localStorage.setItem('profile', JSON.stringify(profile))
@@ -38,34 +30,4 @@ window.addEventListener('load', function() {
         })
         ;
     });
-
-    //retrieve the profile:
-    //   var retrieve_profile = function() {
-    //     var access_token = localStorage.getItem('access_token');
-    //     if (access_token) {
-    //       lock.getUserInfo(access_token, function (err, profile) {
-    //         if (err) {
-    //           return alert('There was an error getting the profile: ' + err.message);
-    //         }
-    //         // Display user information
-    //         show_profile_info(profile);
-    //       });
-    //     }
-    //   };
-    //
-    //   var show_profile_info = function(profile) {
-    //     var avatar = document.getElementById('avatar');
-    //     document.getElementById('nickname').textContent = profile.nickname;
-    //     btn_login.style.display = "none";
-    //     avatar.src = profile.picture;
-    //     avatar.style.display = "block";
-    //     btn_logout.style.display = "block";
-    //   };
-    //
-    //   var logout = function() {
-    //     localStorage.removeItem('access_token');
-    //     window.location.href = "/";
-    //   };
-    //
-    //   retrieve_profile();
 });
