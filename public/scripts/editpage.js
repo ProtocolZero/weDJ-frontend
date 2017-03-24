3295d1d2a82d4ae215c33649f898c41a5be75203
ryanj89 <ryanj89@gmail.com>
2017-03-23 22:52:15 -0600

    updated add playlist flow, need to return PL id when posting new PL to add to the rest of the tables

--- public/scripts/editpage.js
+++ public/scripts/editpage.js
@@ -1,16 +1,20 @@
 const url = "https://www.googleapis.com/youtube/v3/search?q="
 const setQuery = "&type=video&part=snippet&key=AIzaSyCMWuzTs2X2BxnT4PJ7_23YmEHBoLPhTus"
 //$('.go').click((e) => {
-  e.preventDefault()
-  let searchItem = $('#search').val()
-  console.log(searchItem);
-  $.get(`${url}${searchItem}${setQuery}`)
-    .then(results => {
-      const videoResult = results.items[0]
-      const video = {
-        id: videoResult.id.videoId,
-        title: videoResult.snippet.title,
-        image: videoResult.snippet.thumbnails.medium.url
-      }
-    })
-})
+//   e.preventDefault()
+//   let searchItem = $('#search').val()
+//   console.log(searchItem);
+//   $.get(`${url}${searchItem}${setQuery}`)
+//     .then(results => {
+//       const videoResult = results.items[0]
+//       const video = {
+//         id: videoResult.id.videoId,
+//         title: videoResult.snippet.title,
+//         image: videoResult.snippet.thumbnails.medium.url
+//       }
+//     })
+// })
+
+$(() => {
+  console.log('hi');
+});
