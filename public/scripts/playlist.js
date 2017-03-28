ca1846e9794065c4eca6666bb29d7585e391e5f5
DariaDesiderata <daria.calwell@gmail.com>
2017-03-27 18:41:31 -0600

    add playlist auto-play functionality

--- public/scripts/playlist.js
+++ public/scripts/playlist.js
@@ -1,73 +1,70 @@
 var tag = document.createElement('script');
+var pl = []
+const urlArr = window.location.href.split('=')
+const pId = urlArr[1]
+const url = "https://wedj.herokuapp.com"
+const YTurl = "https://www.youtube.com/embed/"
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player
 
-var player;
-var onYouTubeIframeAPIReady
-var playerReady = new Promise((resolve, reject) => {
+function onPlayerStateChange (e){
+  console.log('Changed')
+  console.log(e)
+}
 
-  onYouTubeIframeAPIReady = function() {
-    player = new YT.Player('player', {
-     //  events: {
-     //    'onReady': resolve,
-     //    'onStateChange': onPlayerStateChange
-     //  }
-    });
-    resolve(player)
-  }
-})
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    events: {
+      'onReady': playerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  })
+}
 
-$().ready(() => {
+function playerReady() {
 
- const urlArr = window.location.href.split('=')
- const pId = urlArr[1]
- const url = "https://wedj.herokuapp.com"
- const YTurl = "https://www.youtube.com/embed/"
-
- function changeSong(url) {
+  function changeSong(url) {
    $('#player').attr('src', `${YTurl}url`)
- }
+  }
 
- function addSongs(song) {
-     $('.songinfo').append(
-      `<tr>
-         <td class="songname">
-          ${song.name}
-          <button class="btn waves-effect waves-light change-song right" value="${song.URL}">Play</button>
-         </td>
-         <td>
-           <button class="btn waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
-         </td>
-         <td>
-           <button class="btn waves-effect waves-light"><i class="material-icons">thumb_down</i></button>
-         </td>
-       </tr>`
-     )
- }
+  $(document).on('click', '.change-song', (e) => {
+    $('#player').attr('src', `${YTurl}${e.target.value}`)
+  })
 
-$(document).on('click', '.change-song', (e) => {
-  $('#player').attr('src', `${YTurl}${e.target.value}`)
-})
+  function addSongs(song) {
+   $('.songinfo').append(
+    `<tr>
+       <td class="songname">
+        ${song.name}
+        <button class="btn waves-effect waves-light change-song right" value="${song.URL}">Play</button>
+       </td>
+       <td>
+         <button class="btn waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
+       </td>
+       <td>
+         <button class="btn waves-effect waves-light"><i class="material-icons">thumb_down</i></button>
+       </td>
+     </tr>`
+    )
+  }
 
-  $.get(`${url}/playlist_song/playlist/${pId}`)
-    .then(songs => {
-      var firstSong = null
-      songs.forEach(song => {
-        $.get(`${url}/song/${song.s_id}`)
-          .then(song => {
-            addSongs(song)
-            if (firstSong == null) {
-              firstSong = song.URL
-              $('#player').attr('src', `${YTurl}${firstSong}`)
-            } else {
-              playerReady.then((player) => {
-                console.log("Hello");
-                player.cueVideoById({videoId: `${song.URL}`})
-              })
-            }
-          })
+
+  function getSongs() {
+    $.get(`${url}/playlist_song/playlist/${pId}`)
+      .then(songs => {
+        var firstSong = null
+        songs.forEach(song => {
+          $.get(`${url}/song/${song.s_id}`)
+            .then(song => {
+              addSongs(song)
+              pl.push(song.URL)
+              player.loadPlaylist({playlist: pl})
+            })
+        })
       })
-    })
-})
+    }
+    getSongs()
+}
