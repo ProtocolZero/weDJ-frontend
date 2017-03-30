10f55d31677fe9f7df3a07f9ab806590032e4c35
ryanj89 <ryanj89@gmail.com>
2017-03-30 17:45:39 -0600

    added transition effects to appending song info and current song and styling

--- public/scripts/playlist.js
+++ public/scripts/playlist.js
@@ -13,6 +13,8 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player
 
+$('.songinfo').hide();
+
 function onPlayerStateChange (e){
   var state = player.getPlayerState()
   if ( state == 1 && player.getPlaylistIndex() != 0){
@@ -25,13 +27,16 @@ function onPlayerStateChange (e){
 
    }
    player.loadPlaylist(pl)
-   $('.songinfo').empty()
-	 setCurrentSong(sl[0])
-   sl.forEach(function (song){
-     addSongs(song)
+   $('.songinfo').hide().empty().fadeOut('slow')
+   sl.forEach(function (song, i){
+		 if (i === 0) {
+			setCurrentSong(song)
+		 } else {
+			addSongs(song)
+		 }
    })
    $('.change-song').click(function (e){
-     player.loadPlaylist({playlist: pl , index: $(this).index('.change-song') })
+     player.loadPlaylist({playlist: pl , index: ($(this).index('.change-song') + 1) })
    })
   }
 }
@@ -43,17 +48,16 @@ function addSongs(song) {
       ${song.name}
      </td>
      <td>
-       <button class="btn btn-floating waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
+       <button class="btn btn-floating waves-effect waves-light move-up-queue"><i class="material-icons">arrow_upward</i></button>
      </td>
      <td>
-       <button class="btn btn-floating waves-effect waves-light red"><i class="material-icons">thumb_down</i></button>
+       <button class="btn btn-floating waves-effect waves-light red"><i class="material-icons">arrow_downward</i></button>
      </td>
    </tr>`
-  )
+  ).fadeIn('slow')
 }
 
 function setCurrentSong(song) {
-	console.log(song)
 	$('.current-song').empty().hide().fadeOut('slow')
 	$('.current-song').append(`${song.name}`).fadeIn('slow')
 }
@@ -83,17 +87,17 @@ function playerReady() {
         songs.forEach(song => {
           $.get(`${url}/song/${song.s_id}`)
             .then(song => {
-              addSongs(song)
-							console.log(sl)
 							if (sl.length === 0) {
 								setCurrentSong(song)
+							} else {
+								addSongs(song)
 							}
               pl.push(song.URL)
               sl.push(song)
               if (ind = songs.length - 1) {
 								player.loadPlaylist({playlist: pl})
 								$('.change-song').click(function (e){
-									player.loadPlaylist({playlist: pl , index: $(this).index('.change-song') })
+									player.loadPlaylist({playlist: pl , index: ($(this).index('.change-song') + 1) })
 								})
             	}
             })
