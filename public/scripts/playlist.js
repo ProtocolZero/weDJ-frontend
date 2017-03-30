9e82dca8e9c435896f96c9098a905f3fd598ca71
ryanj89 <ryanj89@gmail.com>
2017-03-30 16:01:40 -0600

    updated playlist style and added current playing song

--- public/scripts/playlist.js
+++ public/scripts/playlist.js
@@ -14,7 +14,6 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player
 
 function onPlayerStateChange (e){
-  console.log('Changed')
   var state = player.getPlayerState()
   if ( state == 1 && player.getPlaylistIndex() != 0){
    var rotation = player.getPlaylistIndex()
@@ -27,6 +26,7 @@ function onPlayerStateChange (e){
    }
    player.loadPlaylist(pl)
    $('.songinfo').empty()
+	 setCurrentSong(sl[0])
    sl.forEach(function (song){
      addSongs(song)
    })
@@ -39,17 +39,23 @@ function addSongs(song) {
  $('.songinfo').append(
   `<tr class="playlist-item">
      <td class="songname">
-      <button class="btn waves-effect waves-light change-song" value="${song.URL}"><i class="material-icons">play_arrow</i></button>
+      <button class="btn btn-floating waves-effect waves-light change-song" value="${song.URL}"><i class="material-icons">play_arrow</i></button>
       ${song.name}
      </td>
      <td>
-       <button class="btn waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
+       <button class="btn btn-floating waves-effect waves-light"><i class="material-icons">thumb_up</i></button>
      </td>
      <td>
-       <button class="btn waves-effect waves-light red"><i class="material-icons">thumb_down</i></button>
+       <button class="btn btn-floating waves-effect waves-light red"><i class="material-icons">thumb_down</i></button>
      </td>
    </tr>`
   )
+}
+
+function setCurrentSong(song) {
+	console.log(song)
+	$('.current-song').empty().hide().fadeOut('slow')
+	$('.current-song').append(`${song.name}`).fadeIn('slow')
 }
 
 function onYouTubeIframeAPIReady() {
@@ -64,7 +70,6 @@ function onYouTubeIframeAPIReady() {
 function changeName (){
   $.get(`${url}/playlist/${pId}`)
   .then(data=>{
-    console.log(data)
     name = data.name
     $('#name').text(name)
   })
@@ -73,26 +78,28 @@ function playerReady() {
 
 
   function getSongs() {
-    $.get(`${url}/playlist_song/playlist/${pId}`)
+    return $.get(`${url}/playlist_song/playlist/${pId}`)
       .then(songs => {
-
-        var firstSong = null
         songs.forEach(song => {
           $.get(`${url}/song/${song.s_id}`)
             .then(song => {
               addSongs(song)
+							console.log(sl)
+							if (sl.length === 0) {
+								setCurrentSong(song)
+							}
               pl.push(song.URL)
               sl.push(song)
-              if (ind = songs.length - 1){
-              player.loadPlaylist({playlist: pl})
-              $('.change-song').click(function (e){
-                player.loadPlaylist({playlist: pl , index: $(this).index('.change-song') })
-              })
-            }
+              if (ind = songs.length - 1) {
+								player.loadPlaylist({playlist: pl})
+								$('.change-song').click(function (e){
+									player.loadPlaylist({playlist: pl , index: $(this).index('.change-song') })
+								})
+            	}
             })
         })
-      })
-    }
+    	})
+	}
     getSongs()
     changeName()
 }
